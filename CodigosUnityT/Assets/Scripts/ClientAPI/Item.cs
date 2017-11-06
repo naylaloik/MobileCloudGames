157f3493f5c4229d4a40d2c93f884c0885906aaa
Nayla Loik <nayloik@hotmail.com>
2017-11-06 15:30:26 -0200

    Entrega projeto

---
+++ CodigosUnityT/Assets/Scripts/ClientAPI/Item.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Item
+{
+    public int ItemID;
+    public string Nome;
+    public int Idade;
+
+    //public int ItemID
+    //{
+    //    get
+    //    { return _itemID; }
+    //    set { _itemID = value; }
+    //}
+
+    //public string Nome
+    //{
+    //    get { return nome; }
+    //    set { nome = value; }
+    //}
+
+    //public string Descricao
+    //{
+    //    get { return _descricao; }
+    //    set { _descricao = value; }
+    //}
+
+    public string Descricao;
+    /*{
+        get { return raridade; }
+        set { raridade = value; }
+    }*/
+
+    public string Origem;
+   /* {
+        get { return danoMaximo; }
+        set { danoMaximo = value; }
+    }*/
+
+    public int TipoItemID;
+   /* {
+        get { return tipoItemID; }
+        set { tipoItemID = value; }
+    }*/
+
+    //private TipoItem _tipoItem;
+
+    //public TipoItem _TipoItem
+    //{
+    //    get { return _tipoItem; }
+    //    set { _tipoItem = value; }
+    //}
+}
